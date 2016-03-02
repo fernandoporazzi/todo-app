be138982e81101bb2c462ea510eb9dc24e5be473
fernandoporazzi <fernandoporazzi@gmail.com>
2016-03-02 20:20:22 -0300

    Develop core of API

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,17 +1,36 @@
 'use strict';
 
-const ItemModel = require('../models/Item.js')
+const ItemModel = require('../models/Item.js');
+const log = require('winston');
 
 module.exports = {
   index : (req, res) => {
-    res.render('home', {
-      title: 'Todo App!'
+    ItemModel.find((err, result) => {
+      if (err != null) {
+        return log.info('Error while fetching initial data');
+      }
+
+      res.render('home', {
+        title: 'Todo App!',
+        todos: JSON.stringify(result)
+      });
     });
   },
 
   save: (req, res) => {
-    res.json({
-      taskname: req.body.name
+    var item = new ItemModel({
+      name: req.body.name,
+      description: req.body.description
+    });
+
+    item.save((err, item) => {
+      if (err) {
+        return res.json({
+          err: 'Error while saving a new item'
+        });
+      }
+
+      res.json({data: 'Item saved!'});
     });
   }
 };
