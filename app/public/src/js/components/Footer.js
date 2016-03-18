b1730b7fb3f92caf4708c4988de877fb206ee3b1
fernandoporazzi <fernandoporazzi@gmail.com>
2016-03-18 05:33:35 -0300

    Implement footer and basic filters

--- app/public/src/js/components/Footer.js
+++ app/public/src/js/components/Footer.js
@@ -3,12 +3,43 @@ import React from 'react';
 export default class Footer extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      filters: ['All', 'Active', 'Completed'],
+      activeFilter: 0
+    };
+  }
+
+  getFiltersHTML() {
+    var filters = this.state.filters.map((item, index) => {
+      var active;
+      if (this.state.activeFilter === index) {
+        active = 'active';
+      } else {
+        active = ''
+      }
+      return (
+        <li key={index}>
+          <span className={active} onClick={this.handleFilterChange.bind(this, item, index)}>{item}</span>
+        </li>
+      );
+    });
+
+    return filters;
+  }
+
+  handleFilterChange(item, index, event) {
+    this.setState({activeFilter: index});
+
+    // Update list
   }
 
   render() {
+    var filters = this.getFiltersHTML()
+
     return (
       <footer className="footer section">
-        footer
+        <ul className="filters">{filters}</ul>
       </footer>
     );
   }
